ee75c59de1636629d5e7bb5d4491c616ca68f3dc
Shawn59 <xxx_respekt.ru@mail.ru>
2020-04-15 09:36:01 +0500

    #master - init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <React.Fragment>
+            <span>Hello</span>
+        </React.Fragment>
+    )
+};
+
+export default App;
