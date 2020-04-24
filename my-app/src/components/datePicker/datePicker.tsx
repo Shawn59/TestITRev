5b581cfe4e9fde6de1e2aaac2552c19a1883c7fe
Shawn59 <xxx_respekt.ru@mail.ru>
2020-04-24 17:36:15 +0500

    #master - переписал компонент датапикер на класс

--- my-app/src/components/datePicker/datePicker.tsx
+++ my-app/src/components/datePicker/datePicker.tsx
@@ -4,7 +4,7 @@ import "react-datepicker/dist/react-datepicker.css";
 import moment from "moment";
 import "./style.css";
 
-export interface IDatePicker {
+export interface IDatePickerProps {
     actionChange: Function,
     selectedDate?: Date,
     invalid: boolean,
@@ -14,7 +14,97 @@ export interface IDatePicker {
     placeholder: string
 }
 
-const MyDatePicker: FC<IDatePicker> = (props) => {
+export interface IDatePickerState {
+    changeValue: string,
+    isInvalid: boolean
+}
+
+class MyDatePicker extends React.PureComponent<IDatePickerProps, IDatePickerState> {
+
+    constructor(props: any) {
+        super(props);
+
+        this.state = {
+            changeValue: (props.selectedDate ? moment(props.selectedDate).format(props.format) : ''),
+            isInvalid: props.invalid
+        }
+    }
+
+    public checkValidDateFormat = (dateStr: string) => {
+        return moment(dateStr, this.props.format, true).isValid();
+    };
+
+    // у momentjs и Date разные форматы
+    public getConvertFormatForDate = (format: string) => {
+        switch (format) {
+            case 'DD.MM.YYYY': {
+                return 'dd.MM.yyyy';
+                break;
+            }
+        }
+    };
+
+    private handleChangeRaw = (event: any) => {
+        if (event) {
+            let dateStr = event.target.value;
+
+            this.setState({
+                isInvalid: !this.checkValidDateFormat(dateStr),
+                changeValue: dateStr
+            });
+        }
+    };
+
+    private handleChange = (date: Date | null, event: any) => {
+        let isValid = this.checkValidDateFormat(event.target.value) || event.target.value === undefined;
+
+        if (date && event && isValid) {
+            this.props.actionChange(date, event);
+
+            this.setState({
+                isInvalid: !isValid
+            });
+        }
+    };
+
+    render() {
+        const {
+            selectedDate,
+            disabled,
+            label,
+            format,
+            placeholder
+        } = this.props;
+        const toDay = new Date();
+
+        return (
+            <Fragment>
+                {label && label.trim() && <span className="label">{label}</span>}
+
+                <DatePicker
+                    selected={selectedDate}
+                    value={selectedDate && !this.state.isInvalid
+                        ? moment(selectedDate).format(format)
+                        : this.state.changeValue
+                    }
+                    onChange={this.handleChange}
+                    placeholderText={placeholder}
+                    dateFormat={this.getConvertFormatForDate(format)}
+                    disabled={disabled}
+                    className={this.state.isInvalid ? "error-style_input" : ""}
+                    onChangeRaw={this.handleChangeRaw}
+                >
+                    <div className="today_block">
+                        <span>{'Сегодня ' + toDay.toLocaleDateString()}</span>
+                    </div>
+                </DatePicker>
+            </Fragment>
+        )
+    }
+
+}
+
+/*const MyDatePicker: FC<IDatePickerProps> = (props) => {
     const {actionChange, selectedDate, invalid, disabled, label, format, placeholder} = props;
 
     const [changeValue, setChangeValue] = React.useState<string>(selectedDate
@@ -22,7 +112,7 @@ const MyDatePicker: FC<IDatePicker> = (props) => {
         : ''
     );
     const [isInvalid, setIsInvalid] = React.useState<boolean>(invalid);
-    const toDay = new Date();
+
 
     const checkValidDateFormat = (dateStr: string) => {
         return moment(dateStr, format, true).isValid();
@@ -46,7 +136,7 @@ const MyDatePicker: FC<IDatePicker> = (props) => {
         }
     };
 
-    const handleChange = (date: Date|null, event: any) => {
+    const handleChange = (date: Date | null, event: any) => {
         let isValid = checkValidDateFormat(event.target.value) || event.target.value === undefined;
         if (date && event && isValid) {
             setIsInvalid(!isValid);
@@ -74,6 +164,6 @@ const MyDatePicker: FC<IDatePicker> = (props) => {
             </DatePicker>
         </Fragment>
     );
-};
+};*/
 
 export default MyDatePicker;
